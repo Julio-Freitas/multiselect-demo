e48ef917028b7e34e2b70a79179a3fc114818341
@julin.freitas <julio.freitas@solides.com.br>
2023-08-11 10:12:45 -0300

    fix: improve deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    outDir: "dist",
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["./.vitest/setup.ts"],
